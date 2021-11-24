8bbc679976f3e4c776f122fc0897718b552b252f
Joshua Kloepfer <jkloepfer20@stuy.edu>
2021-11-24 11:47:50 -0500

    work

--- project01.c
+++ project01.c
@@ -28,7 +28,7 @@ int check_redirect(char * type, char ** argv, int len) {
 			break;
 		}
 	}
-	if (i == len - 1) return 0;
+	if (i == len) return 0;
 	return i;
 }
 
@@ -43,21 +43,40 @@ int main() {
 		char ** argv;
 		int argc = lineparse(&argv, line);
 		char type;
+		int dupin;
+		int dupout;
 		if (line[strlen(line) - 1] == '\n') line[strlen(line) - 1] = '\0';
 		if (strcmp(line, "exit") == 0) return 0;
 		else if (strcmp(argv[0], "cd") == 0) chdir(argv[1]);
+		int i;
+                if (i = check_redirect(&type, argv, argc)) {
+                	if (type == '<') {
+                        	int stdin_new = open(argv[i+1], O_RDONLY);
+                                dupin = dup(0);
+				dup2(stdin_new, 0);
+                        }
+                        else if (type == '>') {
+                        	int stdout_new = open(argv[i+1], O_CREAT | O_TRUNC | O_WRONLY, 0644);
+                        	dupout = dup(1);
+				dup2(stdout_new, 1);
+                        }
+                        int a;
+                        for (a = i; a < argc; a++) {
+                                argv[a] = NULL;
+                        }
+                }
 		int f = fork();
 		if (!f) {
-			if (int i = check_redirect(&type, argv, argc)) {
-                        	if (type == '<') {
-                                	int stdin_new = open(argv[i+1], O_CREAT | O_TRUNC);
-                                	dup2(stdin_new, 0);
-                        	}
-                	}
 			return execvp(argv[0], argv);
 		}
 		else {
 			waitpid(f, &status, 0);
+			if (i && type == '>') {
+				dup2(dupout, 1);
+			}
+			else if (i && type == '<') {
+				dup2(dupin, 0);
+			}
 			free(argv);
 		}
 	}
